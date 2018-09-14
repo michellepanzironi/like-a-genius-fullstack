ddd44558b0d6e084155255ec896c3e4848667ce2
Michelle Panzironi <michelle.panzironi@gmail.com>
2018-09-14 14:14:57 -0400

    mid seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@
 
 require 'faker'
 
+# ----USERS----
+
 User.destroy_all
 
 guest = User.create(
@@ -19,4 +21,57 @@ bootsriley = User.create(
   username: 'bootsriley',
   password: 'bootsriley',
   img_url: 'https://pbs.twimg.com/profile_images/749636414082469888/aC2LO3cR_400x400.jpg'
+)
+
+# ----ARTISTS----
+
+lizzo = Artist.create(
+  name: 'Lizzo',
+  img_url: 'https://imgur.com/SQqOZJJ'
+)
+les_sins = Artist.create(
+  name: 'Les Sins',
+  img_url: 'https://imgur.com/sRi4s6k'
+)
+rupaul = Artist.create(
+  name: 'Rupaul',
+  img_url: 'https://imgur.com/349DGbc'
+)
+capital_cities = Artist.create(
+  name: 'Capital Cities',
+  img_url: 'https://imgur.com/jWFhCd2'
+)
+yeasayer = Artist.create(
+  name: 'Yeasayer',
+  img_url: 'https://imgur.com/1ktPKoZ'
+)
+prince = Artist.create(
+  name: 'Prince',
+  img_url: 'https://imgur.com/3DdiFfb'
+)
+lorde = Artist.create(
+  name: 'Lorde',
+  img_url: 'https://imgur.com/VUAhmt0'
+)
+we_are_scientists = Artist.create(
+  name: 'We Are Scientists',
+  img_url: 'https://imgur.com/jJomNLt'
+)
+kaytranada = Artist.create(
+  name: 'KAYTRANADA',
+  img_url: 'https://imgur.com/OlxWUko'
+)
+david_bowie = Artist.create(
+  name: 'David Bowie',
+  img_url: 'https://imgur.com/2JHlPja'
+)
+
+# ----ALBUMS----
+let_em_say = Album.create(
+  title: 'Let Em Say',
+  artist_id: 1,
+)
+michael = Album.create(
+  title: 'Michael',
+  artist_id: 2,
 )
